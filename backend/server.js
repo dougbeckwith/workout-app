68da568e743fc7d9f7d37d728b51b8fbb521e6f4
Doug Beckwith <dougcbeckwith@gmail.com>
2022-07-06 10:37:43 -0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+require('dotenv').config()
+
+// express app
+const app = express()
+
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+// routes
+app.get('/', (req, res) => {
+  res.json({mssg: 'response message'})
+})
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`)
+})
