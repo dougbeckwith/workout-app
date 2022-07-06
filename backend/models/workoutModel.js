299e12363393dbb52ef85370d4bb6371821f2fa7
Doug Beckwith <dougcbeckwith@gmail.com>
2022-07-06 12:42:05 -0700

    add workout model

---
+++ backend/models/workoutModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+// function to create new schema
+const Schema = mongoose.Schema
+
+// create new schema
+// define the structure of the documents
+const workoutSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    reps: {
+      type: Number,
+      required: true,
+    },
+    load: {
+      type: Number,
+      required: true,
+    },
+  },
+  {timestamps: true}
+)
+
+// Create the model
+// Use the methods on the model to interact with the database
+module.exports = mongoose.model('Workout', workoutSchema)
