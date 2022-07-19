4c4b9069d76bb5d5a3b74100d8c868177acf1e4a
Doug Beckwith <dougcbeckwith@gmail.com>
2022-07-19 12:38:43 -0700

    add comments for useWorkoutsContext hook

--- frontend/src/hooks/useWorkoutsContext.js
+++ frontend/src/hooks/useWorkoutsContext.js
@@ -2,8 +2,15 @@ import {WorkoutContext} from '../context/WorkoutContext'
 import {useContext} from 'react'
 
 export const useWorkoutsContext = () => {
+  // use Context is the hook we imported
+  // we pass in the workout context object
+  // this hook returns to use the value of the context
+  // which is the value we passed into the Provider Component
+  // its this object with the state and the dispatch function
+  // anytime we want to use the data we just invode the useWorkoutsContext hook
   const context = useContext(WorkoutContext)
 
+  // only use the context if its in the right tree of components
   if (!context) {
     throw Error(
       'useWorkoutsContext must be used inside a WorkoutsContextProvider'
