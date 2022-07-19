ab7d256f4fe209bd8dd48c757430be26b6d9e78e
Doug Beckwith <dougcbeckwith@gmail.com>
2022-07-19 12:39:34 -0700

    add delete action and comments

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -2,7 +2,13 @@ import {createContext, useReducer} from 'react'
 
 export const WorkoutContext = createContext()
 
+// state which is the previous state
+// the second argument is the action is the object that is
+// passed into the dispatch function
+// it has a type and a payload
+// easyiest way to do this is write a switch statement
 export const workoutsReducer = (state, action) => {
+  // return the new state we want
   switch (action.type) {
     case 'SET_WORKOUTS':
       return {
@@ -12,21 +18,40 @@ export const workoutsReducer = (state, action) => {
       return {
         workouts: [action.payload, ...state.workouts],
       }
+    case 'DELETE_WORKOUT':
+      return {
+        workouts: state.workouts.filter((workout) => {
+          return workout._id !== action.payload._id
+        }),
+      }
     default:
       return state
   }
 }
 
 export const WorkoutContextProvider = ({children}) => {
-  // dispatch function to update the state
-  // if we want to update the state we call the dispatch function
-  // we pass a object in with a type, and payload
-  // when we call this dispatch function our workouts reducer function is envoked
+  // use reducer takes in two things
+  // reducer function name
+  // initial value for state
+  // we get back the state and the dispatch function to update
+  // the state
 
+  // dispatch function
+  // we pass in a object as an argument
+  // the first property:
+  // type: 'STRING' describe the state change you want to make
+  // the second property:
+  // payload:[{}, {}]   represents any data we need to make the state change
+  // this argument inside the dispatch function is called an action
+  // when we call this dispatch function
+  // inturn our reducer function is invoked which is this workouts Reducer
+  // function and it passes the action into the reducer function
+  // so it can update the state using this data
   const [state, dispatch] = useReducer(workoutsReducer, {
     workouts: null,
   })
   return (
+    // value pass in the state and the dispatch function to update the state
     <WorkoutContext.Provider value={{...state, dispatch}}>
       {children}
     </WorkoutContext.Provider>
