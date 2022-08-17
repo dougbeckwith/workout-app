a441434f75f24acff92743c645b6b09e974c4f37
Doug Beckwith <dougcbeckwith@gmail.com>
2022-08-16 18:22:34 -0700

    add auth headers

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import {useEffect} from 'react'
 import {useWorkoutsContext} from '../hooks/useWorkoutsContext'
+import {useAuthContext} from '../hooks/useAuthContext'
 
 // components
 import WorkoutDetails from '../components/WorkoutDetails'
@@ -7,6 +8,7 @@ import WorkoutForm from '../components/WorkoutForm'
 
 const Home = () => {
   const {workouts, dispatch} = useWorkoutsContext()
+  const {user} = useAuthContext()
 
   useEffect(() => {
     const fetchWorkouts = async () => {
@@ -16,16 +18,21 @@ const Home = () => {
       // it tells our react dev server to proxy any requests it
       // doesnt recognize to localhost 4000
       // FOR PRODUCTION MAKE SURE IT POINTS TO THE PROPER END POINT
-      const response = await fetch('/api/workouts')
+      const response = await fetch('/api/workouts', {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      })
       const json = await response.json()
 
       if (response.ok) {
         dispatch({type: 'SET_WORKOUTS', payload: json})
       }
     }
-
-    fetchWorkouts()
-  }, [dispatch])
+    if (user) {
+      fetchWorkouts()
+    }
+  }, [dispatch, user])
 
   return (
     <div className='home'>
